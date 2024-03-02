2119d489fccfcf7731ebead6509b444a4916ff1b
Melissa Baker <mtfourcandles@gmail.com>
2024-03-02 15:44:50 +0000

    results div & script link

--- richard.js
+++ richard.js
@@ -5,9 +5,9 @@
   });
 
   document.querySelector('#calculate-button').addEventListener('click', function() {
-    let timeToRecharge = document.querySelector('#batteryTemp').value;
-    let costToReplaceBattery = document.querySelector('#chargeTime').value;
-    let totalNumberofBatteriesInUse = document.querySelector('#batteryCost').value;
+    let batteryTemp = document.querySelector('#avgTemp').value;
+        let chargeTime = document.querySelector('#chargeTime').value;
+        let batteryCost = document.querySelector('#batteryCost').value;
   
     calculate(batteryTemp, chargeTime, batteryCost); // Pass the input values to the calculate function
   
