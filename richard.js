e458ff77fa38b644e8976e0676baa6e80f41052b
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-02 15:18:11 +0000

    js update

--- richard.js
+++ richard.js
@@ -4,11 +4,23 @@
     window.location.href = 'index.html';
   });
 
+  document.querySelector('#calculate-button').addEventListener('click', function() {
+    let batteryTemp = document.querySelector('#batteryTemp').value;
+    let chargeTime = document.querySelector('#chargeTime').value;
+    let batteryCost = document.querySelector('#batteryCost').value;
+  
+    calculate(batteryTemp, chargeTime, batteryCost); // Pass the input values to the calculate function
+  
+    document.querySelector('.results').style.display = 'block';
+    document.querySelector('.input-container').style.display = 'none';
+  });
+
 
 
   document.querySelector('#calculate-button').addEventListener('click', function() {
-    document.querySelector('.results').style.display = 'block';
+    document.querySelector('.inputBox').style.display = 'none';
   });
+
   document.querySelector('#calculate-button').addEventListener('click', function() {
     calculate(); // Call the calculate function from calculation.js
     document.querySelector('.results').style.display = 'block';
