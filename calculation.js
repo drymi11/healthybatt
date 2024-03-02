c0162e4f5baee32bd33037137508e732334b2003
BiggestChris <stylianouchristopher8@gmail.com>
2024-03-02 15:38:40 +0000

    First draft of calculation engine

--- calculation.js
+++ calculation.js
@@ -7,39 +7,65 @@ const NyoboltStats = {
 }
 
 // User inputs
-let userTimeInOperation = 300; // Time in operation (minutes)
 let userTimeInRecharge = 30; // Time in recharge (minutes)
-let userTotalNumberOfRechargeCycles = 3000; // Number of recharge cycles until viable
 let userCostToReplace = 4000; // Cost to replace a battery (£)
-let userValueInOperation = 1; // Value in operation (£ / minute)
 let userTotalBatteries = 300; // Total number of batteries in use
-let userHoursOperation = 24; // Hours of operation in an operating day
 
+// HARDCODED AS DUMMY DATA FOR NOW
+let userTimeInOperation = 300; // Time in operation (minutes) - HARDCODED FOR NOW AS DUMMY DATA
+let userTotalNumberOfRechargeCycles = 3000; // Number of recharge cycles until viable - HARDCODED FOR NOW AS DUMMY DATA
+let userValueInOperation = 1; // Value in operation (£ / minute) - HARDCODED FOR NOW AS DUMMY DATA
+let userHoursOperation = 24; // Hours of operation in an operating day - HARDCODED FOR NOW AS DUMMY DATA
+
+
+// Calculate cycle length for user vs Nyobolt
 let userCycleLength = cyclelength(userTimeInOperation, userTimeInRecharge);
 let nyoCycleLength = cyclelength(NyoboltStats['TimeInOperation'], NyoboltStats['TimeInRecharge']);
 
-let cycleLength = userTimeInOperation / userTimeInRecharge;
-let life = userTimeInRecharge * cycleLength;
+// Calculate lifetime
+let userLife = life(userTimeInRecharge, userCycleLength);
+let nyoLife = life(userTimeInRecharge, nyoCycleLength);
 
-let valuePerCycle = userTimeInOperation * userValueInOperation;
-let valuePerMin = valuePerCycle / cycleLength;
-let costPerMin = userCostToReplace / life;
+// Calculate details
+let userValuePerMin = value(userTimeInOperation, userValueInOperation, userCycleLength)
+let nyoValuePerMIn = value(NyoboltStats['TimeInOperation'], userValueInOperation, nyoCycleLength)
 
-let totalValueLifetime = life * valuePerMin;
-let totalCostLifetime = life * costPerMin;
+let userValueLifetime = valueLife(userLife, userValuePerMin);
+let nyoValueLiftime = valueLife(nyoLife, nyoValuePerMin);
 
-let valueMinLife = life / totalValueLifetime;
-let costMinLife = life / totalCostLifetime;
+let userBenefitMinLife = benefitMinLife(userlife, userValueLifetime, userCostToReplace)
+let nyoBenefitMinLife = benefitMinLife(nyolife, nyoValueLifetime, userCostToReplace)
 
-let benefitMinLife = round(valueMinLife - costMinLife, 2);
-let totalBenefitMin = benefitMinLife * userTotalBatteries;
 let minsOperatingDay = userHoursOperation * 60;
-let totalBenefitDay = totalBenefitMin * minsOperatingDay;
 
-let finalResult = returnSum(userTotalBenefitDay, nyoboltTotalBenefitDay)
+let userBenefitDay = totalBenefitDay(userBenefitMin, minsOperatingDay);
+let nyoBenefitDay = totalBenefitDay(nyoBenefitMin, minsOperatingDay);
+
+let finalResult = returnSum(userBenefitDay, nyoboltBenefitDay);
 
 function returnSum(a, b) {
     return b - a;
+}
+
+function totalBenefitDay(totalBenefitMin, minsOperatingDay) {
+    return totalBenefitMin * minsOperatingDay;
+}
+
+function benefitMinLife(life, totalValueLifetime, userCostToReplace) {
+    return round((life / totalValueLifetime) - (life / userCostToReplace), 2);
+}
+
+function valueLife(life, valuePerMin) {
+    return (life * valuePerMin);
+}
+
+function value(userTimeInOperation, userValueInOperation, cycleLength) {
+    return ((userTimeInOperation * userValueInOperation) / cycleLength);
+}
+
+function life(recharge, cyclelength) {
+    let life = recharge * cyclelength;
+    return life;
 }
 
 function cyclelength(op, recharge) {
