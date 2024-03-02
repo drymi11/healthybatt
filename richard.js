5722720ec956b84bf3a6d3ba31505a79ba79d8d5
Quinnlizzy <quin27@hotmail.co.uk>
2024-03-02 15:56:17 +0000

    updated notes

--- richard.js
+++ richard.js
@@ -1,9 +1,11 @@
 
-
+//HOME LINK WHEN ON THE RESULTS SCREEN
   document.querySelector('h1').addEventListener('click', function() {
     window.location.href = 'index.html';
   });
 
+
+//RUNS THE CALCULATION FUNCTION WHEN THE CALCULATE BUTTON IS CLICKED AND REMOVES INPUT BOX FROM SCREEN RENDER
   document.querySelector('#calculate-button').addEventListener('click', function() {
     let batteryTemp = document.querySelector('#avgTemp').value;
         let chargeTime = document.querySelector('#chargeTime').value;
@@ -15,13 +17,14 @@
     document.querySelector('.input-container').style.display = 'none';
   });
 
-
+//REMOVES INPUT BOX FROM SCREEN RENDER
 
   document.querySelector('#calculate-button').addEventListener('click', function() {
     document.querySelector('.inputBox').style.display = 'none';
   });
 
-  document.querySelector('#calculate-button').addEventListener('click', function() {
-    calculate(); // Call the calculate function from calculation.js
-    document.querySelector('.results').style.display = 'block';
-  });
+//TODO
+//TEST OF INPUT AND OUTPUT RENDER
+//RESULTS RENDER TO THE SCREEN FROM THE ALGORITHM
+//THEORY OF OPERATION LINK - CALCULATE BUTTON BECOMES THEORY OF CALCULATIONS BUTTON 
+//THAT IS WHAT ARE THE ASSUMPTIONS MADE BEHIND THE CALCULATION
